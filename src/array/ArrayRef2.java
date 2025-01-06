d9fe416a82dbdbc55071cb5816e473165048bdad
jinjinjala-ish <qhshqhsh312@sch.ac.kr>
2025-01-06 22:31:58 +0900

    25.01.06

---
+++ src/array/ArrayRef2.java
@@ -0,0 +1,12 @@
+package array;
+
+public class ArrayRef2 {
+    public static void main(String[] args) {
+        int[] students = new int[]{90, 80, 70, 60, 50}; //배열 생성과 초기화
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("학생 " + (i+1) + "의 점수: " + students[i]);
+        }
+
+    }
+}
